45769305778c61205c1ee1a9036aaee051e2133a
pawan67 <pawantamada8@gmail.com>
2022-04-11 15:15:29 +0530

    minor changes

--- components/MusicList.js
+++ components/MusicList.js
@@ -25,7 +25,7 @@ const MusicList = ({ data }) => {
   }
   return (
     <div
-      data-aos="zoom-in"
+      data-aos="flip-down"
       onClick={() => setSongNum(data.id - 1)}
       className=" hover:bg-base-200 transition-all font-semibold flex items-center cursor-pointer rounded-xl w-full h-16 md:px-5   my-2"
     >
